f38312d25568a0f446a080544d15d1f05704ebe7
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2016-01-01 12:55:06 +0530

    Fixed the test suite

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,68 +1,63 @@
-class GildedRose
-
-  def initialize(items)
-    @items = items
-  end
-
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+def update_quality(items)
+  items.each do |item|
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      if item.quality > 0
+        if item.name != 'Sulfuras, Hand of Ragnaros'
+          item.quality -= 1
+        end
+      end
+    else
+      if item.quality < 50
+        item.quality += 1
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality += 1
+            end
           end
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality += 1
+            end
+          end
+        end
+      end
+    end
+    if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.sell_in -= 1
+    end
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+          if item.quality > 0
+            if item.name != 'Sulfuras, Hand of Ragnaros'
+              item.quality -= 1
+            end
+          end
+        else
+          item.quality = item.quality - item.quality
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality += 1
         end
       end
     end
   end
 end
 
-class Item
-  attr_accessor :name, :sell_in, :quality
+# DO NOT CHANGE THINGS BELOW -----------------------------------------
 
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
+Item = Struct.new(:name, :sell_in, :quality)
 
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
-end
+# We use the setup in the spec rather than the following for testing.
+#
+# Items = [
+#   Item.new("+5 Dexterity Vest", 10, 20),
+#   Item.new("Aged Brie", 2, 0),
+#   Item.new("Elixir of the Mongoose", 5, 7),
+#   Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+#   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+#   Item.new("Conjured Mana Cake", 3, 6),
+# ]
+
